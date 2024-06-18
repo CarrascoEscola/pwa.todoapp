bf0c674b78400de6f3d23d1d45164e531932e242
André Carrasco Escola <161333906+CarrascoEscola@users.noreply.github.com>
2024-06-18 11:21:27 -0300

    Add files via upload

---
+++ pwa-todo-app/Cazz/index.php
@@ -0,0 +1,20 @@
+<?php 
+
+$nome = "fernado";
+$cidade = "Cianorte";
+$telefone ="44 99999 7070";
+
+?>
+
+<html>
+<head>
+    <title><?php echo $nome ?></title>
+</head>
+
+
+<body>
+    <h1><?phpecho "Ola meu nome é ".$nome;?></h1>
+    <p>Meu telefone é <?php echo $telefone;?> <br>
+    Eu moro em <?php eho $cidade;?></p>
+</body>
+</html>
